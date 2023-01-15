d32d7fd456e47de26c138fea6f46743a1a9508ff
Kevin Barrantes <kabarrantes163@gmail.com>
2023-01-15 13:48:26 -0600

    Control flow
    Using control flow functions (like if and for statements) inside the HTML code.

--- webApp.py
+++ webApp.py
@@ -6,13 +6,12 @@ app = Flask(__name__)
 #Creating a basic root page, this decorator lets us specify the route's path
 @app.route('/')
 def index():
-    user_name = "Kevin Barrantes"
-    letters = list(user_name)
-    puppy_dictionary = {'puppy_name' : "Fluffy"}
-    #Using parameters on the render template function to pass on data to the html template,\
+    example_list = [1,2,3,4]
+    example_bool = True
+    #Using parameters on the render template function to pass on data to the html template,
     #similar to Angular's data interpolation.
-    return render_template('basic.html', user_name=user_name,letters=letters,
-                            puppy_dictionary = puppy_dictionary)
+    return render_template('basic.html', example_list=example_list,
+                            example_bool=example_bool)
 
 
 if __name__ == '__main__':
