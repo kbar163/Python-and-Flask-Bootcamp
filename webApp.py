cb4123739bb4f63f9c28427c7399270bf79e6ce5
Kevin Barrantes <kabarrantes163@gmail.com>
2023-01-15 13:25:14 -0600

    Jinja templating + template variables
    Create python variables inside the Flask application and pass them on to the HTML files using Jinja template variables. Similar to Angular's data interpolation practice.

--- webApp.py
+++ webApp.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template
 
+#Initializing the Flask application
 app = Flask(__name__)
 
+#Creating a basic root page, this decorator lets us specify the route's path
 @app.route('/')
 def index():
-    return render_template('basic.html')
+    user_name = "Kevin Barrantes"
+    letters = list(user_name)
+    puppy_dictionary = {'puppy_name' : "Fluffy"}
+    #Using parameters on the render template function to pass on data to the html template,\
+    #similar to Angular's data interpolation.
+    return render_template('basic.html', user_name=user_name,letters=letters,
+                            puppy_dictionary = puppy_dictionary)
 
-@app.route('/test')
-def page2():
-    return '<h1>This is page two</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
