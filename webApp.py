b982b9ea15bdcc08f605ecd926f47127f7c9b3a8
Kevin Barrantes <kabarrantes163@gmail.com>
2023-01-18 15:45:15 -0600

    Navigating through pages, custom 404 page
    Navigating and sharing data through pages in a live session, adding a custom 404 page when an user tries to navigate to an unknown route.

--- webApp.py
+++ webApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 #Initializing the Flask application
 app = Flask(__name__)
@@ -6,12 +6,23 @@ app = Flask(__name__)
 #Creating a basic root page, this decorator lets us specify the route's path
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/puppy/<name>')
-def puppy_name(name):
-    return render_template('puppy.html',name=name)
+@app.route("/signup")
+def signup_form():
+    return render_template('signup.html')
+
+@app.route("/thankyou")
+def thank_you():
+    first_name = request.args.get("first_name")
+    return render_template('thankyou.html',first_name=first_name)
+
+@app.errorhandler(404)
+def page_not_foud(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
