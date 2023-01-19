6e8eb0fdba782ead74685c61e057d32b5bb1930a
Kevin Barrantes <kabarrantes163@gmail.com>
2023-01-19 08:34:17 -0600

    Form fields and validators examples

--- webApp.py
+++ webApp.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import (StringField,SubmitField,BooleanField,
+                    RadioField,SelectField,TextAreaField)
+from wtforms.validators import DataRequired
 
 #Initializing the Flask application
 app = Flask(__name__)
@@ -8,23 +10,39 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 
 # Creating a flask form by creating a class that inherits from FlaskForm
 class InfoForm(FlaskForm):
-
-    breed = StringField("What is the breed of your dog?")
-    breed_submit = SubmitField("Submit")
+    #using the data required validator, autochecks on submit.
+    breed = StringField("What is the breed of your dog?",validators=[DataRequired()]) 
+    neutered = BooleanField("Is your dog neutered?")
+    temper = RadioField("How would you describe the temper of your dog?",
+                        choices=[("Docile","Docile"),("Defensive","Defensive")])
+    food_choice = SelectField(u"Pick your dog's favourite food:",
+                             choices=[("Dry food","Dry food"),("Wet food","Wet food"),("Mixed food","Mixed food")])
+    feedback = TextAreaField()
+    submit = SubmitField("Submit")
 
 
 #Creating a basic root page, this decorator lets us specify the route's path. The methods parameter allows
 #to pass the form to the template.
 @app.route('/',methods=['GET','POST'])
 def index():
-    breed = False
+    
     form = InfoForm()
 
-    if form.validate_on_submit():
-        breed = form.breed.data
-        form.breed.data = ''
+    if form.validate_on_submit(): #if all the data validation checks pass on submit, redirect to a thank you page
+        session["breed"] = form.breed.data
+        session["neutered"] = form.neutered.data
+        session["temper"] = form.temper.data
+        session["food_choice"] = form.food_choice.data
+        session["feedback"] = form.feedback.data
+        flash("Example flash message")
+        return redirect(url_for("thank_you")) 
 
-    return render_template('index.html',form=form,breed=breed)
+    return render_template("index.html",form=form)
+
+@app.route("/thankyou")
+def thank_you():
+    return render_template("thankyou.html")
+
 
 @app.errorhandler(404)
 def page_not_foud(e):
